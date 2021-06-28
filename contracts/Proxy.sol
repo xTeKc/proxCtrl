57568452875c15ee5858fa96cf42c880bb99e03e
xTeKc <bcd3v@pm.me>
2021-06-28 08:39:16 -0400

    add assembly to fallback func

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -19,6 +19,16 @@ contract Proxy is Storage {
         address implementation = currentAddress;
         require(currentAddress != address(0));
         bytes memory data = msg.data;
+
+        assembly {
+            let result := delegatecall(gas, implementation, add(data, 0x20), mload(data), 0, 0)
+            let size := returndatasize
+            let ptr := mload(0x40);
+            returndatacopy(ptr, 0, size)
+            switch result
+            case 0 {revert(ptr, size)}
+            default {return(ptr, size)}
+        }
     }    
 
 }
