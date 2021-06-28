0b58a9e8db0d3ee42dffc37f2e0c7f32a2ee35dd
xTeKc <bcd3v@pm.me>
2021-06-28 08:19:26 -0400

    create storage contract

---
+++ contracts/Storage.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.0;
+
+contract Storage {
+
+    mapping (string => uint256) _uintStorage;
+    mapping (string => address) _addressStorage;
+    mapping (string => bool) _boolStorage;
+    mapping (string => string) _stringStorage;
+    mapping (string => bytes4) _bytesStorage;
+    address public owner;
+    bool public _initialized;
+
+}
